586793c4dce08313c8ea8382e23979268131220e
Sean O'Connor <seanoc5@gmail.com>
2024-03-25 15:47:59 -0400

    minor change

---
+++ src/main/groovy/com/oconeco/corpusminder/test.groovy
@@ -0,0 +1,10 @@
+package com.oconeco.corpusminder
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+
+Logger log = LogManager.getLogger(this.class.name);
+
+log.info "test"
+
